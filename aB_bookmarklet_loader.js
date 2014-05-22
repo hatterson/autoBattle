46d6b60bc5c29fa6cd08bc3b80a77118d5d271b2
Sean Patterson <hatterson@gmail.com>
2014-05-22 15:15:22 -0400

    Create aB_bookmarklet_loader.js

---
+++ aB_bookmarklet_loader.js
@@ -0,0 +1,9 @@
+javascript:(
+    function () {
+        var js = document.createElement('script');
+        js.setAttribute('type', 'text/javascript');
+        js.setAttribute('id', 'autoBattleScript');
+        js.setAttribute('src', 'https://raw.githubusercontent.com/hatterson/autoBattle/master/autoBattle.js');
+        document.head.appendChild(js);
+    }()
+);
