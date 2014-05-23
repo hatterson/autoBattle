ebfdcc48ab1726a9124a536839a96bae38660d3d
Sean Patterson <hatterson@gmail.com>
2014-05-22 23:31:34 -0400

    Move to github.io
    Raw causes error in newer browsers due to mime type.  IO/pages is the proper loading

--- aB_bookmarklet_loader.js
+++ aB_bookmarklet_loader.js
@@ -3,7 +3,7 @@ javascript:(
         var js = document.createElement('script');
         js.setAttribute('type', 'text/javascript');
         js.setAttribute('id', 'autoBattleScript');
-        js.setAttribute('src', 'https://raw.githubusercontent.com/hatterson/autoBattle/master/autoBattle.js');
+        js.setAttribute('src', 'http://hatterson.github.io/autoBattle/autoBattle.js');
         document.head.appendChild(js);
     }()
 );
